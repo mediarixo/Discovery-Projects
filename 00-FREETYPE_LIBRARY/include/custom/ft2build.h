be888d5c0200645296460e442d53f631669d8e43
catadoo <rpastorina@gmail.com>
2022-04-21 20:38:31 +0200

    Add files via upload

---
+++ 00-FREETYPE_LIBRARY/include/custom/ft2build.h
@@ -0,0 +1,9 @@
+#ifndef __FT2_BUILD_KEIL_ARM_H__
+#define __FT2_BUILD_KEIL_ARM_H__
+
+#define FT_CONFIG_OPTIONS_H  <custom/my-ftoption.h>
+#define FT_CONFIG_MODULES_H  <custom/my-ftmodule.h>
+
+#include <freetype/config/ftheader.h>
+
+#endif /* __FT2_BUILD_KEIL_ARM_H__ */
